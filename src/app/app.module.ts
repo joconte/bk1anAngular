db4d05ac7575618a6ddbec8b84609c2171d82c56
joconte <etud@contejonathan.com>
2020-01-10 21:27:43 +0100

    added notif + lower timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,53 @@ import { HomeComponent } from './component/home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NotifierModule, NotifierOptions} from 'angular-notifier';
+
+
+/**
+ * Custom angular notifier options
+ */
+const customNotifierOptions: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: 'left',
+      distance: 12
+    },
+    vertical: {
+      position: 'bottom',
+      distance: 12,
+      gap: 10
+    }
+  },
+  theme: 'material',
+  behaviour: {
+    autoHide: 5000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
+};
+
 
 @NgModule({
   declarations: [
@@ -18,7 +65,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FontAwesomeModule,
     BrowserAnimationsModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    NotifierModule.withConfig(customNotifierOptions)
   ],
   exports: [
   MatProgressBarModule
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -3,6 +3,7 @@ import {CodeValideOuiNon} from '../../model/codeValideOuiNon';
 import {TestedCodeResult} from '../../model/testedCodeResult';
 import {CodeService} from '../../service/code.service';
 import { faCopy, faCheckCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import {NotifierService} from 'angular-notifier';
 import {interval} from 'rxjs';
 
 @Component({
@@ -21,20 +22,22 @@ export class HomeComponent implements OnInit {
   faMinusCircle = faMinusCircle;
   @ViewChild('tooltipSpan', null) tooltipSpan: ElementRef;
 
-  constructor(private codeService: CodeService) {}
+  constructor(private codeService: CodeService, private notifier: NotifierService) {}
 
   async ngOnInit() {
+    this.codeValideOuiNon = new CodeValideOuiNon();
     this.winnerCodes = new Array();
+    this.stats = new TestedCodeResult();
     await this.getUntestedCode();
-    interval(1000).subscribe(x => this.weHaveAWinner());
-    interval(1000).subscribe(x => this.getStats());
-    interval(1000).subscribe(x => this.checkMyCodeIsUntested());
+    this.stats = await this.codeService.getStats() as TestedCodeResult;
+    await this.weHaveAWinner();
+    interval(60000).subscribe(x => this.weHaveAWinner());
+    interval(10000).subscribe(x => this.getStats());
+    interval(30000).subscribe(x => this.checkMyCodeIsUntested());
   }
 
   async checkMyCodeIsUntested() {
     const untested = await this.codeService.checkMyCodeIsUntested(this.codeValideOuiNon) as boolean;
-    console.log(untested);
-
     if (!untested) {
       await this.getUntestedCode();
     }
@@ -54,7 +57,18 @@ export class HomeComponent implements OnInit {
   }
 
   async getStats() {
-    this.stats = await this.codeService.getStats() as TestedCodeResult;
+    const newStats = await this.codeService.getStats() as TestedCodeResult;
+    /*
+    let numberOfNewTested = 0;
+    if (newStats.tested > this.stats.tested) {
+      numberOfNewTested = newStats.tested - this.stats.tested;
+      if (numberOfNewTested === 1) {
+        this.notifier.notify('success', '1 nouveau code testé par les kheys !');
+      } else {
+        this.notifier.notify('success', numberOfNewTested + ' nouveaux codes testés par les kheys !');
+      }
+    }*/
+    this.stats = newStats;
   }
 
   async weHaveAWinner() {
@@ -73,6 +87,7 @@ export class HomeComponent implements OnInit {
     selBox.select();
     document.execCommand('copy');
     document.body.removeChild(selBox);
+    this.notifier.notify('success', 'Code ' + this.codeValideOuiNon.code + ' copié dans le presse papier');
   }
 
   onMouseMove(e) {
@@ -81,6 +96,4 @@ export class HomeComponent implements OnInit {
     this.tooltipSpan.nativeElement.style.top = (y + 20) + 'px';
     this.tooltipSpan.nativeElement.style.left = (x + 20) + 'px';
   }
-
-
 }
